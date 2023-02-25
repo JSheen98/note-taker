058e68761891369f9293568a81a8520bccba6508
Jsheen98 <jsheendev@gmail.com>
2023-02-24 19:15:16 -0700

    Added functionality for html routing

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+// Declarations
+const notes = require('express').Router()
+const wildcard = require('express').Router()
+const path = require('path')
+
+// // TODO: GET /notes request should return notes.html
+notes.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/notes.html'))
+})
+
+// // TODO: GET * (wildcard) request should return the index.html
+wildcard.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+module.exports = { notes, wildcard }
