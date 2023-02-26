99e5565c233f1f35de0ebf7a3554b35cdcd28b1d
Jsheen98 <jsheendev@gmail.com>
2023-02-26 12:52:33 -0700

    Added the first part of my delete request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,7 @@ const apiNotes = require('express').Router()
 const db = require('../db/db.json')
 const uuid = require('uuid')
 const fs = require('fs')
+const dbParsed = JSON.parse(fs.readFileSync('./db/db.json'))
 
 // Reads db file
 apiNotes.get('/notes/', (req, res) => {
@@ -26,13 +27,29 @@ apiNotes.post('/notes/:id', (req, res) => {
             const parsedData = JSON.parse(data)
             parsedData.push(newNote)
             fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
-            err ? console.err(err) : console.info(`\nData written`))
+                err ? console.error(err) : console.info(`\nData written`))
         }
     })
 })
 
 // BONUS TODO: DELETE /api/notes/:id should receive a query param containing id of note to delete. In order to delete a note, you need to read all notes from db.json, remove note with given id, and rewrite notes to db.json
+apiNotes.delete('/notes/:id', (req, res) => {
+    // const dbParsed = JSON.parse(fs.readFile('./db/db.json')) 
+    const id = req.params.id * 1
+    const noteToDelete = dbParsed.find(el => el.id === id)
+    const index = dbParsed.indexOf(noteToDelete)
 
+    dbParsed.splice(index, 1)
+
+    fs.writeFile('./db/db.json', JSON.stringify(dbParsed, null, 4), (err) => 
+        res.status(204).json({
+            status: "success",
+            data: {
+                note: null
+            }
+        })
+    )
+})
 
 
 
